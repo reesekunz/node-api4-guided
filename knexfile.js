4d03f7838ea68bbb7f7ef662537cae326083c29a
Reese Kunz <reesekunz21@gmail.com>
2019-09-05 10:37:34 -0700

    added production config

--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,14 @@ module.exports = {
     connection: {
       filename: './data/shouts.db3',
     },
+    // copy deployment object and change it to production - heroku is looking for 
+    // production not development (can tell from heroku logs on terminal)
+    production: {
+      client: 'sqlite3',
+      useNullAsDefault: true,
+      connection: {
+        filename: './data/shouts.db3',
+      },
     pool: {
       afterCreate: (conn, done) => {
         conn.run('PRAGMA foreign_keys = ON', done);
