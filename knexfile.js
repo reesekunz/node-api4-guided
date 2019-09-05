cc86ad298bb1f7b65daa55269d763001b3d2ef24
Reese Kunz <reesekunz21@gmail.com>
2019-09-05 10:51:57 -0700

    fixed deployment error

--- knexfile.js
+++ knexfile.js
@@ -1,28 +1,42 @@
 module.exports = {
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     useNullAsDefault: true,
     connection: {
-      filename: './data/shouts.db3',
+      filename: "./data/shouts.db3"
     },
-    // copy deployment object and change it to production - heroku is looking for 
+    // copy deployment object and change it to production - heroku is looking for
     // production not development (can tell from heroku logs on terminal)
-    production: {
-      client: 'sqlite3',
-      useNullAsDefault: true,
-      connection: {
-        filename: './data/shouts.db3',
-      },
     pool: {
       afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done);
-      },
+        conn.run("PRAGMA foreign_keys = ON", done);
+      }
     },
     migrations: {
-      directory: './data/migrations',
+      directory: "./data/migrations"
     },
     seeds: {
-      directory: './data/seeds',
-    },
+      directory: "./data/seeds"
+    }
   },
+  // copy deployment object and change it to production - heroku is looking for
+  // production not development (can tell from heroku logs on terminal)
+  production: {
+    client: "sqlite3",
+    useNullAsDefault: true,
+    connection: {
+      filename: "./data/shouts.db3"
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      }
+    },
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  }
 };
