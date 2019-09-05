18135befd72b67b20262dc8f56be592e5478f28b
Reese Kunz <reesekunz21@gmail.com>
2019-09-05 09:48:10 -0700

    heroku environment defined

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const server = require('./api/server.js');
+// from https://www.npmjs.com/package/dotenv
+// need to load before we use environment, so this line needs to be at the top of index.js
+require("dotenv").config();
+
+const server = require("./api/server.js");
 
 const port = 4000;
 server.listen(port, () => {
